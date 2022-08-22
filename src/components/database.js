0c0483e961d53ff2a40735160d28133e7231e525
ramesh-choudhary-ext <ramesh.choudhary.ext@extendaretail.com>
2022-08-22 16:05:46 +0200

    feat: initial commit

---
+++ src/components/database.js
@@ -0,0 +1,66 @@
+export const Leaderboard = [
+    {
+        name: "Priyanka Guggari",
+        teamName: "Team Champion",
+        score: 1550,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "1"
+    },
+    {
+        name: "Rameez Dalvi",
+        teamName: "Team Looser",
+        score: 2310,
+        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "1"
+    },
+    {
+        name: "Ramesh Choudhary",
+        teamName: "Team Moderate",
+        score: 2100,
+        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "1"
+    },
+    {
+        name: "Priyanka Guggari",
+        teamName: "Team Champion",
+        score: 2100,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "2"
+    },
+    {
+        name: "Rameez Dalvi",
+        teamName: "Team Looser",
+        score: 1550,
+        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "2"
+    },
+    {
+        name: "Ramesh Choudhary",
+        teamName: "Team Moderate",
+        score: 2310,
+        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "2"
+    },
+    {
+        name: "Priyanka Guggari",
+        teamName: "Team Champion",
+        score: 2310,
+        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "3"
+    },
+    {
+        name: "Rameez Dalvi",
+        teamName: "Team Looser",
+        score: 2100,
+        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "3"
+    },
+    {
+        name: "Ramesh Choudhary",
+        teamName: "Team Moderate",
+        score: 1550,
+        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+        gameId: "3"
+    }
+
+]
