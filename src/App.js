ca561c3702b6d661862a5e8e1a20d6d7d78faf06
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-08-24 00:51:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Board from './components/board';
 import './components/style.css';
-import background from './img/placeholder.png'; 
+import background from './img/placeholder.png';
 
 function App() {
   return (
-    <div className="App" id='main' style={{ backgroundImage: `url(${background})` }}>
+    <div className="App" id='main' style={{ backgroundImage: `url(${background})`, height: "100%", }}>
       <Board></Board>
     </div>
   );
