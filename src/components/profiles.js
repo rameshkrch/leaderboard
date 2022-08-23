ad95f70de880244e38b590882c69e9c1a80d5005
Lagnashree <98232757+Lagnashree@users.noreply.github.com>
2022-08-24 00:50:12 +0200

    Update profiles.js

--- src/components/profiles.js
+++ src/components/profiles.js
@@ -1,4 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import IconButton from '@mui/material/IconButton';
 
 export default function profiles({ Leaderboard }) {
     return (
@@ -7,35 +19,55 @@ export default function profiles({ Leaderboard }) {
         </div>
     )
 }
-
 function Item(data) {
+
     return (
-
         <>
-            {
-                data.map((value, index) => (
-                    <div className="flex" key={index}>
-                        <div className="item">
-                            <img src={value.img} alt="" />
+            <Box sx={{ width: '100%', maxWidth: 1000 }}>
+                <TableContainer component={Paper}>
+                    <Table sx={{ maxWidth: 800 }}>
+                        <TableBody>
+                            {
+                                data.map((value, index) => (
+                                    <div className="flex" key={index}>
+                                        <TableRow key={index}>
+                                            <TableCell style={{ width: 500 }} align="left">
+                                                <div className="item">
+                                                    <img src={value.img} alt="" />
 
-                            <div className="info">
-                                <h3 className='name text-dark'>{value.name}</h3>
-                                <span>{value.teamName}</span>
-                            </div>
-                        </div>
-
-                        <div className="game">
-                            <span>{value.gameId}</span>
-                        </div>
-                        <div className="item">
-                            <span>{value.score}</span>
-                        </div>
-                    </div>
-                )
-                )
-            }
+                                                    <div className="info">
+                                                        <h3 className='name text-dark'>{value.name}</h3>
+                                                        <span>{value.teamName}</span>
+                                                    </div>
+                                                </div>
+                                            </TableCell>
+                                            <TableCell style={{ width: 140 }} align="left">
+                                                <Typography variant="h6" gutterBottom>
+                                                    {value.gameId}
+                                                </Typography>
+                                            </TableCell>
+                                            <TableCell style={{ width: 210 }} align="right">
+                                                <Stack spacing={2} direction="row">
+                                                    <TextField label="Add Score" focused />
+                                                    <IconButton color="primary" aria-label="add to shopping cart">
+                                                        <AddCircleIcon fontSize="large" />
+                                                    </IconButton>
+                                                </Stack>
+                                            </TableCell>
+                                            <TableCell style={{ width: 240 }} align="left">
+                                                <Typography variant="h4" gutterBottom>
+                                                    {value.score}
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                    </div>
+                                )
+                                )
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </>
-
-
     )
 }
