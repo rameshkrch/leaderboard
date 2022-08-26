0b6fc235e61087147f31a22eb591af3b1ea1f19a
ramesh-choudhary-ext <ramesh.choudhary.ext@extendaretail.com>
2022-08-26 16:09:13 +0200

    feat: add image url

--- src/components/database.js
+++ src/components/database.js
@@ -1,23 +1,23 @@
 export const Leaderboard = [
     {
         name: "Old Monks",
-        teamName: "Pramod|Ashutosh|Sachin|Saket",
+        teamName: "Pramod | Ashutosh | Sachin | Saket",
         teamId: 1,
         score: 1000,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_1.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140427Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=1671a6e8c6593cc78ad773af1feb2904c10f8e5dd767f76753e96bf0ca5036ea",
         gameId: "1"
     },
     {
         name: "Fabulous 4",
-        teamName: "Minhaj|Amit|Deepak|Sovana",
+        teamName: "Minhaj | Amit | Deepak | Sovana",
         teamId: 2,
         score: 500,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_2.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140505Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=ac35890c016115988df0e428ab0c9e75435c53fd92f04d99ea52d8e454dac792",
         gameId: "1"
     },
     {
         name: "F.R.I.E.N.D.S.",
-        teamName: "Mahesh|Shwetha|Uzma|Bhavya",
+        teamName: "Mahesh | Shwetha | Uzma | Bhavya",
         teamId: 3,
         score: 0,
         img: "",
@@ -25,7 +25,7 @@ export const Leaderboard = [
     },
     {
         name: "Mantis Swarm",
-        teamName: "Ashish|Smaranika|Gregory|Claudia",
+        teamName: "Ashish | Smaranika | Gregory | Claudia",
         teamId: 4,
         score: 0,
         img: "",
@@ -33,7 +33,7 @@ export const Leaderboard = [
     },
     {
         name: "Ninjas in Pyjamas",
-        teamName: "Hans|ichelle|Prashanth|Nabanita",
+        teamName: "Hans | ichelle | Prashanth | Nabanita",
         teamId: 5,
         score: 0,
         img: "",
@@ -41,7 +41,7 @@ export const Leaderboard = [
     },
     {
         name: "Old Monks",
-        teamName: "Pramod|Ashutosh|Sachin|Saket",
+        teamName: "Pramod | Ashutosh | Sachin | Saket",
         teamId: 1,
         score: 0,
         img: "",
@@ -49,7 +49,7 @@ export const Leaderboard = [
     },
     {
         name: "Fabulous 4",
-        teamName: "Minhaj|Amit|Deepak|Sovana",
+        teamName: "Minhaj | Amit | Deepak | Sovana",
         teamId: 2,
         score: 0,
         img: "",
@@ -57,7 +57,7 @@ export const Leaderboard = [
     },
     {
         name: "F.R.I.E.N.D.S.",
-        teamName: "Mahesh|Shwetha|Uzma|Bhavya",
+        teamName: "Mahesh | Shwetha | Uzma | Bhavya",
         teamId: 3,
         score: 0,
         img: "",
@@ -65,7 +65,7 @@ export const Leaderboard = [
     },
     {
         name: "Mantis Swarm",
-        teamName: "Ashish|Smaranika|Gregory|Claudia",
+        teamName: "Ashish | Smaranika | Gregory | Claudia",
         teamId: 4,
         score: 0,
         img: "",
@@ -73,7 +73,7 @@ export const Leaderboard = [
     },
     {
         name: "Ninjas in Pyjamas",
-        teamName: "Hans|ichelle|Prashanth|Nabanita",
+        teamName: "Hans | ichelle | Prashanth | Nabanita",
         teamId: 5,
         score: 0,
         img: "",
@@ -81,7 +81,7 @@ export const Leaderboard = [
     },
     {
         name: "Old Monks",
-        teamName: "Pramod|Ashutosh|Sachin|Saket",
+        teamName: "Pramod | Ashutosh | Sachin | Saket",
         teamId: 1,
         score: 0,
         img: "",
@@ -89,7 +89,7 @@ export const Leaderboard = [
     },
     {
         name: "Fabulous 4",
-        teamName: "Minhaj|Amit|Deepak|Sovana",
+        teamName: "Minhaj | Amit | Deepak | Sovana",
         teamId: 2,
         score: 0,
         img: "",
@@ -97,7 +97,7 @@ export const Leaderboard = [
     },
     {
         name: "F.R.I.E.N.D.S.",
-        teamName: "Mahesh|Shwetha|Uzma|Bhavya",
+        teamName: "Mahesh | Shwetha | Uzma | Bhavya",
         teamId: 3,
         score: 0,
         img: "",
@@ -105,7 +105,7 @@ export const Leaderboard = [
     },
     {
         name: "Mantis Swarm",
-        teamName: "Ashish|Smaranika|Gregory|Claudia",
+        teamName: "Ashish | Smaranika | Gregory | Claudia",
         teamId: 4,
         score: 0,
         img: "",
@@ -113,7 +113,7 @@ export const Leaderboard = [
     },
     {
         name: "Ninjas in Pyjamas",
-        teamName: "Hans|ichelle|Prashanth|Nabanita",
+        teamName: "Hans | ichelle | Prashanth | Nabanita",
         teamId: 5,
         score: 0,
         img: "",
@@ -121,7 +121,7 @@ export const Leaderboard = [
     },
     {
         name: "Old Monks",
-        teamName: "Pramod|Ashutosh|Sachin|Saket",
+        teamName: "Pramod | Ashutosh | Sachin | Saket",
         teamId: 1,
         score: 0,
         img: "",
@@ -129,7 +129,7 @@ export const Leaderboard = [
     },
     {
         name: "Fabulous 4",
-        teamName: "Minhaj|Amit|Deepak|Sovana",
+        teamName: "Minhaj | Amit | Deepak | Sovana",
         teamId: 2,
         score: 0,
         img: "",
@@ -137,7 +137,7 @@ export const Leaderboard = [
     },
     {
         name: "F.R.I.E.N.D.S.",
-        teamName: "Mahesh|Shwetha|Uzma|Bhavya",
+        teamName: "Mahesh | Shwetha | Uzma | Bhavya",
         teamId: 3,
         score: 0,
         img: "",
@@ -145,7 +145,7 @@ export const Leaderboard = [
     },
     {
         name: "Mantis Swarm",
-        teamName: "Ashish|Smaranika|Gregory|Claudia",
+        teamName: "Ashish | Smaranika | Gregory | Claudia",
         teamId: 4,
         score: 0,
         img: "",
@@ -153,7 +153,7 @@ export const Leaderboard = [
     },
     {
         name: "Ninjas in Pyjamas",
-        teamName: "Hans|ichelle|Prashanth|Nabanita",
+        teamName: "Hans | ichelle | Prashanth | Nabanita",
         teamId: 5,
         score: 0,
         img: "",
@@ -161,42 +161,42 @@ export const Leaderboard = [
     },
     {
         name: "Old Monks",
-        teamName: "Pramod|Ashutosh|Sachin|Saket",
+        teamName: "Pramod | Ashutosh | Sachin | Saket",
         teamId: 1,
         score: 0,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_1.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140427Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=1671a6e8c6593cc78ad773af1feb2904c10f8e5dd767f76753e96bf0ca5036ea",
         gameId: "5"
     },
     {
         name: "Fabulous 4",
-        teamName: "Minhaj|Amit|Deepak|Sovana",
+        teamName: "Minhaj | Amit | Deepak | Sovana",
         teamId: 2,
         score: 0,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_2.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140505Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=ac35890c016115988df0e428ab0c9e75435c53fd92f04d99ea52d8e454dac792",
         gameId: "5"
     },
     {
         name: "F.R.I.E.N.D.S.",
-        teamName: "Mahesh|Shwetha|Uzma|Bhavya",
+        teamName: "Mahesh | Shwetha | Uzma | Bhavya",
         teamId: 3,
         score: 0,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_3.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140527Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=aea2b8dc72a09e92429897bcd8177cf1378e9e21e54ec5671dd54c5392123a97",
         gameId: "5"
     },
     {
         name: "Mantis Swarm",
-        teamName: "Ashish|Smaranika|Gregory|Claudia",
+        teamName: "Ashish | Smaranika | Gregory | Claudia",
         teamId: 4,
         score: 0,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_4.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140608Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=be4f28216d1ef34ba503886c5132d2ebce97d53f4a117b953402dc532d589904",
         gameId: "5"
     },
     {
         name: "Ninjas in Pyjamas",
-        teamName: "Hans|ichelle|Prashanth|Nabanita",
+        teamName: "Hans | Michelle | Prashanth | Nabanita",
         teamId: 5,
         score: 0,
-        img: "",
+        img: "https://leaderboard-img.s3.eu-north-1.amazonaws.com/img/Team_5.JPG?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEF4aDGV1LWNlbnRyYWwtMSJHMEUCIETNjOKHSvqKvPP3zHxG9ciymc4ADo291byt9Y%2B0%2FJ5ZAiEAg%2ByHSs%2FAwXmma5csEbuyOstD%2F48dn%2BjtaPv3PdeXg0Mq7QII1%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgw0MzY1NjYyNzAyNjUiDFU6xMKAO6z8cisYJCrBAgR%2FdlO8k0YORmFuUYh0yQh%2B7vvcNTpxDnYUMiOnqZEX9S5b8gRvGjuqt4tAAi3cu7Dx%2BzoX8hhRhfrwR%2BGNqvHXKGvxbDtXAW0WHiQHTaT6VWwVtV7D1RhEmaKNfRHCvZlRL9aqhJxBrv%2Fs3%2BME4GfRptm040U31n%2Beg42s4kzg75a9GtNmrmWqTM8nKh9Z4Z9pR6ISgIRzmQ0eD0r6NgRnwwZmQ4bp9alPvK%2F%2BL6MLXg6i%2FNB1yGqf66iAubEA0pryS%2FEhaHHtrp%2FKKFK9BFsoQ6TCiD42Mb4gS0VUUe5P733eKyC1a7ZgGi1TbWRf%2FRk3zfLviHQWw5LWYNam2XZbD4DOfUeaWDIeGx1SjcZT9%2FMznQNffmsNu1NS7FfqTq1juk6n%2BlF1ywRnbnMbMaZb1njIFlYwarRGiJ9fDYhu8DDE3KKYBjqzAoIJIcpH6izSNGQIHARB%2B9llK77DFs%2BoUlLiK7c1Qq6mx1EP9BY4mmr%2FOHrPYf%2FsEAfTp0C3%2BYWcjIKZLEK2nvTnoS6%2BmcUC8hldrMQojXMG2TUVyASLPOeU6fGhrPSUG%2BoWPaNRwLDou73SWuidH2804uoUM5f2oktZGDNJCTaVONYXDznWYU4TIuDumhnvtChxkKj1qkDNcxwMSAAonT8w8Yw6EbiAWZ192zV5DJFiQoRoS7zIhOPmjnSC7VnTKB1ykYOeoIUTRh32Pi0KJgi%2FbR2rySUusngzBjALhX6ypmzfwU5sUsywnKvm0FJjKCyqpu2G19oR8OcxIvUcF2uLFkukUDAB2XvNHIblP%2B79CV9DTCyX5%2FNyTQ8QNCgRA2CObxQaoiDq8RMmdBxhDmHsssQ%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220826T140624Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAWLJLAUE4TULEFI5N%2F20220826%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Signature=b5eb6bf548447512b5aadafc05eadd3c507bf1a96a43d05e82d4b6e665cd0f54",
         gameId: "5"
     }
 
